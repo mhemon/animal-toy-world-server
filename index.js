dca8c0212c7d4025cb8c74fb974b99b43ecbf8c2
mhemon <mhemon02@gmail.com>
2023-05-18 20:57:29 +0600

    category routes added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
@@ -6,6 +8,51 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntvgsob.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+
+        const galleryCollection = client.db("animalToy").collection('gallery');
+
+        const toyCollection = client.db("animalToy").collection('toy');
+
+        app.get('/gallery', async (req, res) => {
+            const cursor = galleryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/category', async (req, res) => {
+            const subcat = req.query.subcategory
+            const cursor = toyCollection.find({"subCategory": subcat});
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Animal Toy is running...')
