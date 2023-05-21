163068870bb72fabfd24b8fcac971a5a77598746
mhemon <mhemon02@gmail.com>
2023-05-21 17:28:03 +0600

    jwt added to secure api on mytoy and add toy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
@@ -20,6 +22,22 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access!' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ error: true, message: 'unauthorized access!' })
+        }
+        req.decoded = decoded
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +46,13 @@ async function run() {
         const galleryCollection = client.db("animalToy").collection('gallery');
 
         const toyCollection = client.db("animalToy").collection('toy');
+
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1h'});
+            res.send({token})
+        })
 
         app.get('/gallery', async (req, res) => {
             const cursor = galleryCollection.find();
@@ -49,7 +74,11 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/mytoys', async (req, res) => {
+        app.get('/mytoys', verifyJWT, async (req, res) => {
+            const decoded = req.decoded
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: true, message: 'forbidden access!' })
+            }
             let query = {};
             if (req.query?.email) {
                 query = { sellerEmail: req.query.email }
@@ -91,7 +120,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/addtoys', async (req, res) => {
+        app.post('/addtoys', verifyJWT, async (req, res) => {
             const added = req.body
             const result = await toyCollection.insertOne(added)
             res.send(result);
